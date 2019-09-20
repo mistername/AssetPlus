6226ceaabf343d52c80472165c7c9d6918cc9c38
mistername <36573936+mistername@users.noreply.github.com>
2019-09-21 01:51:20 +0200

    Initial commit

---
+++ assetplus.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System.IO;
+
+namespace AssetPlus {
+    /// <summary>
+    /// Simple class for adding all the individual of AssetPlus together
+    /// </summary>
+    public class AssetPlus
+    {
+        internal void Awake()
+        {
+            SoundPlus.SoundAwake();
+            FontPlus.FontAwake();
+            TextPlus.LanguageAwake();
+        }
+
+        internal static string[] GetFiles(string Extension) {
+            return Directory.GetFiles(Paths.PluginPath, "*." + Extension, SearchOption.AllDirectories);
+        }
+    }
+}
